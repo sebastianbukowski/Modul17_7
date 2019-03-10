8dab5d61b2a51f822b4fa853b12330cafc1ff922
SebastianBukowski <seb-27@o2.pl>
2019-03-10 16:28:32 +0100

    Add google login

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '425700416899-dplrkjkho3v57l68f4mhjj3ps77l5ko0.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'uA00fPDHVxhk-tzfVVFnIdYw',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};  
